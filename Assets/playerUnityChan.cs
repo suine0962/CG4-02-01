de6554590b48fc03fe8891de4a5608de2afbeca8
裕斗 近藤 <hilotono@icloud.com>
2024-06-15 23:38:10 +0900

    a

--- Assets/playerUnityChan.cs
+++ Assets/playerUnityChan.cs
@@ -32,48 +32,35 @@ public class PlayerScript : MonoBehaviour
     {
         if  (true)
         {
-
-            // 移動速度を取得
             float speed = Input.GetButton("Fire3") ? sprintSpeed : normalSpeed;
 
-            // カメラの向きを基準にした正面方向のベクトル
             Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
 
-            // 前後左右の入力（WASDキー）から、移動のためのベクトルを計算
-            // Input.GetAxis("Vertical") は前後（WSキー）の入力値
-            // Input.GetAxis("Horizontal") は左右（ADキー）の入力値
-            // Vector3 moveZ = cameraForward * Input.GetAxis("Vertical") * speed;  //　前後（カメラ基準）　 
-            Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal") * speed; // 左右（カメラ基準）
+            Vector3 moveX = Camera.main.transform.right * Input.GetAxis("Horizontal") * speed; 
 
-            // isGrounded は地面にいるかどうかを判定します
-            // 地面にいるときはジャンプを可能に
             if (con.isGrounded)
             {
                 moveDirection = moveX;
-                //anim.SetBool("JumpMotion2", true);
-                //anim.SetBool("JumpMotion", false);
                 if (Input.GetButtonDown("Jump"))
                 {
                     moveDirection.y = jump;
-                    //anim.SetBool("JumpMotion", true);
-                    //anim.SetBool("JumpMotion2", false);
                 }
             }
             else
             {
-                // 重力を効かせる
+               
                 moveDirection = moveX + new Vector3(0, moveDirection.y, 0);
                 moveDirection.y -= gravity * Time.deltaTime;
 
             }
 
-            // 移動のアニメーション
+          
             anim.SetFloat("MoveSpeed", (moveX).magnitude);
 
-            // プレイヤーの向きを入力の向きに変更　
+            
             transform.LookAt(transform.position + moveX);
 
-            // Move は指定したベクトルだけ移動させる命令
+           
             con.Move(moveDirection * Time.deltaTime);
         }
     }
